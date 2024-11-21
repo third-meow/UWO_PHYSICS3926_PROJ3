957bd2c2a8a9887f441c29bf079e224552689460
third-meow <jsheaandcats@gmail.com>
2024-11-20 20:24:11 -0500

    adapt pendulum dydt function for this white dwarf ODE

--- SheaJohn_Project3.py
+++ SheaJohn_Project3.py
@@ -1,15 +1,17 @@
 
-# derivative function for pendulum ode, split into 3
-def pend_dydt(t,y,Q,A,omega_d):
-    theta = y[0]
-    phi = y[1]
-    omega = y[2]
 
-    dydt = [0, 0, 0]
-    dydt[0] = omega
-    dydt[1] = omega_d
-    dydt[2] = A*np.cos(phi) - (omega/Q + np.sin(theta))
-    return dydt
+def gamma_func(x):
+    return (x ** 2) / (3 * ((1 + x ** 2) ** (1/2)))
+
+def whitedrawf_dydr(r_bold,ystate):
+    rho_bold = ystate[0]
+    m_bold = ystate[1]
+    x = rho_bold ** (1/3)
+
+    dydr = [0, 0]
+    dydr[0] = -m_bold*rho_bold/(gamma(x)*r*r)
+    dydr[1] = r*r*rho_bold
+    return dydr
 
 # solve with Q=20, A=0.3, wd=2 from theta_m=30
 pend_solution = scipy.integrate.solve_ivp(pend_dydt, [0, 30], [deg_to_rad(30), 0, 0], args=(20,0.3,2))
