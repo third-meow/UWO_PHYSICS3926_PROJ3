95b53bf02722102452ea7b234317e2eb67bf8b8b
third-meow <jsheaandcats@gmail.com>
2024-11-22 18:06:06 -0500

    remove unused code and add section labels

--- SheaJohn_Project3.py
+++ SheaJohn_Project3.py
@@ -14,34 +14,32 @@ M0 = 5.67e33 / (mu_e ** 2)
 rho0 = 9.74e5 * mu_e
 
 def gamma_func(x):
+    """
+    Computes the gamma function of x
+    """
     return (x ** 2) / (3 * ((1 + x ** 2) ** (1/2)))
 
 def whitedrawf_dydr(t, ystate):
-    # you wouldn't think this nessassry in the presence of a zero-checking
-    # termination event but it is just trust me
-    try:
-        if ystate[0] < 0:
-            dydr = [0, 0]
-            return dydr
-
-        # give sensible names
-        r_bold = t
-        rho_bold = ystate[0]
-        m_bold = ystate[1]
-
-        # calculate x
-        x = rho_bold ** (1/3)
-
+    if ystate[0] < 0:
         dydr = [0, 0]
-
-        # calculate derivatives
-        dydr[0] = -m_bold*rho_bold/(gamma_func(x)*(r_bold ** 2))
-        dydr[1] = (r_bold ** 2) * rho_bold
-
-        # return derivatives array
         return dydr
-    except RuntimeWarning:
-        print(f"RunTimeWarning w/ ystate = {ystate}, dydr = {dydr}")
+
+    # give sensible names
+    r_bold = t
+    rho_bold = ystate[0]
+    m_bold = ystate[1]
+
+    # calculate x
+    x = rho_bold ** (1/3)
+
+    dydr = [0, 0]
+
+    # calculate derivatives
+    dydr[0] = -m_bold*rho_bold/(gamma_func(x)*(r_bold ** 2))
+    dydr[1] = (r_bold ** 2) * rho_bold
+
+    # return derivatives array
+    return dydr
 
 # checks for density going to zero and terminates solve_ivp process
 def density_zero_event(t, y):
@@ -54,7 +52,7 @@ if __name__ == '__main__':
     # initial rho values
     res_n = 10
     rho_c = np.logspace(np.log10(1e-1), np.log10(2.5e6), res_n)
-    #rho_c = np.linspace(1e-1, 2.5e6, res_n)
+
     final_mass_bold = np.zeros(res_n)
     final_rad_bold = np.zeros(res_n)
 
@@ -75,7 +73,6 @@ if __name__ == '__main__':
     fig1 = plt.figure(f"figure")
     ax1 = fig1.add_subplot()
     ax1.set_title("Radius by Mass")
-    # plot each intial condition
     ax1.scatter(final_mass, final_rad, label='not usre')
     ax1.set_xlabel("Mass (g)")
     ax1.set_ylabel("Radius (cm)")
@@ -86,11 +83,12 @@ if __name__ == '__main__':
     radius_of_the_sun_in_cm = 6.95508e10
     estimated_Mch_g = final_mass[-1]
     estimated_Mch_MoS = final_mass[-1] / mass_of_sun_in_grams
+    print("\nPART 2 - Estimating Chandrasekhar Limit")
     print(f"Estimated Chandrasekhar Limit : {estimated_Mch_MoS*(mu_e**2)} / (μe)^2 (solar masses)")
     print("Kippenhahn & Weigert (1990) cite Chandrasekhar Limit as 5.836/(μe)^2  (solar masses)")
 
 
-    ## PART 3 Compare intergration methods
+    ## PART 3 Compare integration methods
 
     DOP853_final_mass_bold = np.zeros(3)
     DOP853_final_rad_bold = np.zeros(3)
@@ -110,29 +108,21 @@ if __name__ == '__main__':
     DOP853_final_mass = DOP853_final_mass_bold * M0
     DOP853_final_rad = DOP853_final_rad_bold * r0
 
-    print(DOP853_final_rad)
-    print(DOP853_final_mass)
-    print(final_mass[:3])
-    print(final_rad[:3])
-
+    print("\nPART 3 - Method comparison RK45 vs DOP853")
     # number of sig figs to print
     n_sf = 4
     for i in range(3):
         print(f"With rho_c = {rho_c[i]}:")
-        print(f"RK45 Method produced:\t\tradius={final_rad[i]:.{n_sf}} mass={final_mass[i]:.{n_sf}}")
-        print(f"DOP853 Method produced:\t\tradius={DOP853_final_rad[i]:.{n_sf}} mass={DOP853_final_mass[i]:.{n_sf}}")
+        print(f"\tRK45 Method produced:\t\tradius={final_rad[i]:.{n_sf}} mass={final_mass[i]:.{n_sf}}")
+        print(f"\tDOP853 Method produced:\t\tradius={DOP853_final_rad[i]:.{n_sf}} mass={DOP853_final_mass[i]:.{n_sf}}")
 
     ## Part 4 - Observed data vs model
-
     observed = np.genfromtxt('wd_mass_radius.csv', delimiter=',')[1:]
     fig1 = plt.figure(f"figure")
     ax1 = fig1.add_subplot()
     ax1.set_title("Radius by Mass : Observed and Simulated")
-    # plot each initial condition
     ax1.scatter(final_mass/mass_of_sun_in_grams, final_rad/radius_of_the_sun_in_cm, label='Simulated')
-
     ax1.errorbar(observed[:,0], observed[:,2], xerr=observed[:,1], yerr=observed[:,3], fmt='ro', capsize=5, label='Observed')
-
     ax1.set_xlabel("Mass (solar masses)")
     ax1.set_ylabel("Radius (solar radii)")
     ax1.legend()
